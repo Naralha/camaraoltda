e79a32e31e9618e0f31850b91a0428bd67ad93f9
vitor.rodrigues <vitor.rodrigues>
2021-02-27 17:12:57 -0300

    nova versao da carga

--- Backend/camaraoltda/deploy_files/carga_pesada.sql
+++ Backend/camaraoltda/deploy_files/carga_pesada.sql
@@ -1,75 +1,70 @@
-insert into
-    usuario
-values
-    (1, 'teste.url', '30', 'Bill Gates');
+insert into usuario values (1,'teste.url','30','Bill Gates');
+insert into usuario values (2,'teste.url','32','Steve Jobs');
+insert into usuario values (3,'teste.url','37','Ellon Musk');
+insert into usuario values (4,'teste.url','38','Miley Cyrus');
+insert into usuario values (5,'teste.url','39','John Petruci');
 
-insert into
-    usuario
-values
-    (2, 'teste.url', '32', 'Steve Jobs');
+INSERT INTO public.area(
+	id_area, nome_area)
+	VALUES (1, 'TI');
+INSERT INTO public.area(
+	id_area, nome_area)
+	VALUES (2, 'Business');
+INSERT INTO public.area(
+	id_area, nome_area)
+	VALUES (3, 'Engenharia');
+	
+	
+INSERT INTO public.curriculo(
+	id_curriculo, id_area, id_usuario)
+	VALUES (1, 1, 1);
+INSERT INTO public.curriculo(
+	id_curriculo, id_area, id_usuario)
+	VALUES (2, 2, 2);
+INSERT INTO public.curriculo(
+	id_curriculo, id_area, id_usuario)
+	VALUES (3, 3, 3);
+INSERT INTO public.curriculo(
+	id_curriculo, id_area, id_usuario)
+	VALUES (4, 2, 4);
+INSERT INTO public.curriculo(
+	id_curriculo, id_area, id_usuario)
+	VALUES (5, 1, 5);
+	
 
-insert into
-    usuario
-values
-    (3, 'teste.url', '37', 'Elon Musk');
+INSERT INTO public.competencia(
+	id_competencia, descricao, tipo, id_area)
+	VALUES (1, 'Java', 'S', 1);
+INSERT INTO public.competencia(
+	id_competencia, descricao, tipo, id_area)
+	VALUES (2, 'Spring', 'C', 1);
+INSERT INTO public.competencia(
+	id_competencia, descricao, tipo, id_area)
+	VALUES (3, 'Struts', 'C', 1);
+INSERT INTO public.competencia(
+	id_competencia, descricao, tipo, id_area)
+	VALUES (4, 'Java EE', 'C', 1);
 
-insert into
-    usuario
-values
-    (4, 'teste.url', '38', 'Miley Cyrus');
-
-insert into
-    usuario
-values
-    (5, 'teste.url', '39','John Petrucci');
-
-INSERT INTO
-    public.curriculo(id_curriculo, id_usuario)
-VALUES
-    (1, 1);
-
-INSERT INTO
-    public.curriculo(id_curriculo, id_usuario)
-VALUES
-    (2, 2);
-
-INSERT INTO
-    public.curriculo(id_curriculo, id_usuario)
-VALUES
-    (3, 3);
-
-INSERT INTO
-    public.curriculo(id_curriculo, id_usuario)
-VALUES
-    (4, 4);
-
-INSERT INTO
-    public.curriculo(id_curriculo, id_usuario)
-VALUES
-    (5, 5);
-
-INSERT INTO
-    public.area(id_area, nome_area)
-VALUES
-    (1, 'Engenharia');
-
-INSERT INTO
-    public.area_interesse(id_area_interesse, id_area)
-VALUES
-    (1, 1);
-
-INSERT INTO public.curriculo_areainteresse(
-	id_curriculo, id_area_interesse)
-	VALUES (1, 1);
-INSERT INTO public.curriculo_areainteresse(
-	id_curriculo, id_area_interesse)
-	VALUES (2, 1);
-INSERT INTO public.curriculo_areainteresse(
-	id_curriculo, id_area_interesse)
-	VALUES (3, 1);
-INSERT INTO public.curriculo_areainteresse(
-	id_curriculo, id_area_interesse)
-	VALUES (4, 1);
-INSERT INTO public.curriculo_areainteresse(
-	id_curriculo, id_area_interesse)
-	VALUES (5, 1);
+	
+INSERT INTO public.curriculo_competencia(
+	id_curriculo, id_competencia)
+	VALUES (1, 2);
+INSERT INTO public.curriculo_competencia(
+	id_curriculo, id_competencia)
+	VALUES (1, 3);
+INSERT INTO public.curriculo_competencia(
+	id_curriculo, id_competencia)
+	VALUES (5, 4);
+	
+	
+INSERT INTO public.relacionamento_competencia(
+	id_relacionamento_competencia, id_filho, id_pai)
+	VALUES (1, 2, 1);
+INSERT INTO public.relacionamento_competencia(
+	id_relacionamento_competencia, id_filho, id_pai)
+	VALUES (2, 3, 1);
+INSERT INTO public.relacionamento_competencia(
+	id_relacionamento_competencia, id_filho, id_pai)
+	VALUES (3, 4, 1);
+	
+	

1ef2ea7dbaf35d9bcb356c2a5b7006b1216cf526
Vitor Rodrigues <vitor.rodrigues@outsmartdigital.com.br>
2021-04-01 18:56:55 -0600

    alteracoes.

--- Backend/camaraoltda/src/main/java/br/com/projeto/camaraoltda/security/ILoginAuthService.java
+++ Backend/camaraoltda/src/main/java/br/com/projeto/camaraoltda/security/ILoginAuthService.java
@@ -1,6 +1,8 @@
 package br.com.projeto.camaraoltda.security;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+
 public interface ILoginAuthService {
 
-	String autenticarExternalLogin(OAuth2Entity oAuth2Entity);
+	Payload autenticarExternalLogin(OAuth2Entity oAuth2Entity);
 }
--- Backend/camaraoltda/src/main/java/br/com/projeto/camaraoltda/security/LoginAuthService.java
+++ Backend/camaraoltda/src/main/java/br/com/projeto/camaraoltda/security/LoginAuthService.java
@@ -3,13 +3,22 @@ package br.com.projeto.camaraoltda.security;
 import org.springframework.stereotype.Service;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.gson.Gson;
 
 @Service
 public class LoginAuthService implements ILoginAuthService{
-	private final String CLIENT_ID = "814563106968-8fqjehu1ajcn9dfho9d9i2nfqpnpg76h.apps.googleusercontent.com";
-
+	private final static String CLIENT_ID = "814563106968-8fqjehu1ajcn9dfho9d9i2nfqpnpg76h.apps.googleusercontent.com";
+	private static final String APPLICATION_NAME = "Google+ Java Token Verification";
+	private static final HttpTransport TRANSPORT = new NetHttpTransport();
+	private static final JacksonFactory JSON_FACTORY = new JacksonFactory();
+	private static final Gson GSON = new Gson();
+	
+	
 	@Override
-	public String autenticarExternalLogin(OAuth2Entity oAuth2Entity) {
+	public GoogleIdToken.Payload autenticarExternalLogin(OAuth2Entity oAuth2Entity) {
 		TokenStatus idStatus = new TokenStatus();
         if (oAuth2Entity.getTokenId() != null) {
           // Check that the ID Token is valid.
@@ -23,10 +32,11 @@ public class LoginAuthService implements ILoginAuthService{
             idStatus.setId("");
             idStatus.setMessage("Invalid ID Token.");
           } else {
-            idStatus.setValid(true);
+        	idStatus.setValid(true);
             String gplusId = (String)jwt.get("sub");
             idStatus.setId(gplusId);
             idStatus.setMessage("ID Token is valid.");
+            return jwt;
           }
         } else {
           idStatus.setMessage("ID Token not provided");
